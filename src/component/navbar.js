1b634849e6292b11f54f9e76f2c6774dd6d94863
fadumasaidcodes <fadumasaid2022@gmail.com>
2023-03-19 09:22:32 +0000

    added the navbar section in the app.js

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -1,64 +1,36 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
-function Navbar() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  function handleToggleMenu() {
-    setIsMenuOpen(!isMenuOpen);
-  }
-
+export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
       <div className="container">
-        <NavbarBrand brandName="Portfolio" />
-        <NavbarToggler handleToggleMenu={handleToggleMenu} />
-        <CollapsibleMenu isMenuOpen={isMenuOpen} />
+        <NavLink className="navbar-brand js-scroll-trigger" to="/">Portfolio</NavLink>
+        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+          data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+          aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarResponsive">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <NavLink exact className="nav-link js-scroll-trigger" to="/">Home</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link js-scroll-trigger" to="#About">About</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link js-scroll-trigger" to="#portfolio">Portfolio</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link js-scroll-trigger" to="#skills">Skills</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link js-scroll-trigger" to="#Contact">Contact</NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
 }
 
-function NavbarBrand({ brandName }) {
-  return (
-    <a className="navbar-brand js-scroll-trigger" href="#page-top">{brandName}</a>
-  );
-}
-
-function NavbarToggler({ handleToggleMenu }) {
-  return (
-    <button className="navbar-toggler navbar-toggler-right" type="button" onClick={handleToggleMenu} aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-  );
-}
-
-function CollapsibleMenu({ isMenuOpen }) {
-  return (
-    <div className={`collapse navbar-collapse${isMenuOpen ? ' show' : ''}`} id="navbarResponsive">
-      <ul className="navbar-nav ml-auto">
-        <MenuLink linkText="Home" targetSection="#home" />
-        <MenuLink linkText="About" targetSection="#about" />
-        <MenuLink linkText="Portfolio" targetSection="#portfolio" />
-        <MenuLink linkText="Skills" targetSection="#skills" />
-        <MenuLink linkText="Contact" targetSection="#contact" />
-      </ul>
-    </div>
-  );
-}
-
-function MenuLink({ linkText, targetSection }) {
-  return (
-    <li className="nav-item">
-      <Link className="nav-link js-scroll-trigger" to={targetSection}>{linkText}</Link>
-    </li>
-  );
-}
-
-function Section({ id, children }) {
-  return (
-    <section id={id}>
-      {children}
-    </section>
-  );
-}
