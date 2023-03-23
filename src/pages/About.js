d6e836d74795257f6fc7e62bc99af61ce06c85fe
fadumasaidcodes <fadumasaid2022@gmail.com>
2023-03-23 11:07:55 +0000

    added the about section

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import Navbar from '../component/Navbar'
 
 const About = () => {
   return (
     <div>
-      
+     < Navbar/>
+     <header className="masthead">
+    <div className="container align-me" id="About">
+      <div className="row align-items-center">
+        <div className="col-lg-3 my-auto mb-5">
+          <div className="device-container">
+            <img src="public/images/faduma.png" class="img-fluid rounded-circle" alt="pictur"/>
+          </div>
+        </div>
+        <div className="col-lg-6 my-auto text-center mt-5 mb-5">
+          <h1 className="sub-heading mb-4">About Me</h1>
+          <p className="about-me-txt">
+            I'm a MSc graduate, and I have a passion for building websites and bringing ideas to life through modern and responsive web design using various technologies. I'm a hardworking individual who is honest, sincere, trustworthy, and responsible for the assigned task. You're welcome to check out my work or follow me on my social media accounts.</p>
+          <a href="#Contact" class="btn btn-primary">Contact Me</a>
+        </div>
+      </div>
+    </div>
+  </header>
     </div>
   )
 }
