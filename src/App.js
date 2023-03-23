f9c03de854fa355ef2b4902887996d4fe72efd3e
fadumasaidcodes <fadumasaid2022@gmail.com>
2023-03-23 11:05:27 +0000

    added the navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { HashRouter as Router, Routes } from 'react-router-dom';
-import Navbar from './component/navbar';
+import Navbar from './component/Navbar';
+import About from './pages/About';
 
 export default function App() {
   return (
     <Router>
         <Navbar />
+        <About />
         <Routes>
     
         </Routes>
--- src/component/navbar.js
+++ src/component/navbar.js
@@ -1,35 +1,40 @@
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-export default function Navbar() {
+function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-      <div className="container">
-        <NavLink className="navbar-brand js-scroll-trigger" to="/">Portfolio</NavLink>
-        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
-          data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
-          aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarResponsive">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <NavLink exact className="nav-link js-scroll-trigger" to="/">Home</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link js-scroll-trigger" to="#About">About</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link js-scroll-trigger" to="#portfolio">Portfolio</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link js-scroll-trigger" to="#skills">Skills</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link js-scroll-trigger" to="#Contact">Contact</NavLink>
-            </li>
-          </ul>
-        </div>
+    <div className="container">
+      <NavLink className="navbar-brand js-scroll-trigger" to="/">Portfolio</NavLink>
+      <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+        data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+        aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarResponsive">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <NavLink exact className="nav-link js-scroll-trigger " to="/">Home</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link js-scroll-trigger" to="#About">About</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link js-scroll-trigger" to="#portfolio">Portfolio</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link js-scroll-trigger" to="#skills">Skills</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link js-scroll-trigger" to="#Contact">Contact</NavLink>
+          </li>
+        </ul>
       </div>
-    </nav>
+    </div>
+  </nav>
+
+    
   );
 }
+
+export default Navbar;
