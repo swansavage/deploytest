69ac92bbe3dbe8e0431fe6742e7b2e073139e159
fadumasaidcodes <fadumasaid2022@gmail.com>
2023-03-24 15:31:36 +0000

    added the project.json

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,11 +1,25 @@
-import React from 'react'
-
-const Project = () => {
+export default function projects({ image, title, description, deployed, github, large }) {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className={large ? "mb-4 col-xl-8": "mb-4 col-md-6 col-xl-4"}>
+      <div className="card h-100">
 
-export default Project
+        <div className="p-4" style={{backgroundPosition: "center top", backgroundImage: `backhround-color:green;, url(${process.env.PUBLIC_URL}/images/${image})`, backgroundSize:"cover"}}>
+          
+          {/* image for the card */}
+          <img src={process.env.PUBLIC_URL + "/images/" + image} className="card-img-top" alt="..." />
+        </div>
+        <div className="card-body d-flex flex-column justify-content-around">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">
+            <span className="small">{description}</span>
+          </p>
+          <div>
+            <a href={deployed} className="btn btn-outline-danger mr-2" target="_blank" rel="noreferrer" style={{position:"static" }}>Deployed site</a>
+            <a href={github} className="btn btn-outline-danger ml-2" target="_blank" rel="noreferrer" style={{position:"static" }}>Github repo</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
+}
