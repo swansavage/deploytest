61b9ba3b89e644dce40ffdd85af1c56661351c66
fadumasaidcodes <fadumasaid2022@gmail.com>
2023-03-24 14:41:57 +0000

    added the latest

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { HashRouter as Router, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './component/Navbar';
 import About from './pages/About';
 import Home from './pages/Home'
 
 export default function App() {
   return (
-    <Router>
-        <Navbar />
-        <About />
-        <Home />
-        <Routes>
-    
-        </Routes>
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route path="Navbar" element={<Navbar />} />
+          <Route path="About" element={<About />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
