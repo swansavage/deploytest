198c214c82702999f125f77efa5f174046137fd5
fadumasaidcodes <fadumasaid2022@gmail.com>
2023-03-24 14:49:42 +0000

    added the latest changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Navbar from './component/Navbar';
 import About from './pages/About';
 import Home from './pages/Home'
@@ -8,8 +9,8 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}>
-          <Route path="Navbar" element={<Navbar />} />
+      <Route path="/" element={<Navbar />}>
+          <Route index element={<Home />} />
           <Route path="About" element={<About />} />
         </Route>
       </Routes>
