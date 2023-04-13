c1f987a6c32a5b9f88fdb52981b6fe1bc8171896
fadumasaidcodes <fadumasaid2022@gmail.com>
2023-04-13 09:12:59 +0100

    added the latest changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,7 @@ import displayImg from "../component/images/faduma.png"
 
 const About = () => {
   return (
-    <div>
+    <div className='about-img' >
   
      <header className="masthead">
     <div className="container align-me" id="About">
@@ -19,7 +19,7 @@ const About = () => {
           </div>
         </div>
         <div className="col-lg-6 my-auto text-center mt-5 mb-5">
-          <h1 className="sub-heading mb-4">About Me</h1>
+          <h1 className="sub-heading  about-heading ">About Me</h1>
           <p className="about-me-txt">
             I'm a MSc graduate, and I have a passion for building websites and bringing ideas to life through modern and responsive web design using various technologies. I'm a hardworking individual who is honest, sincere, trustworthy, and responsible for the assigned task. You're welcome to check out my work or follow me on my social media accounts.</p>
           <a href="#Contact" class="btn btn-primary">Contact Me</a>
